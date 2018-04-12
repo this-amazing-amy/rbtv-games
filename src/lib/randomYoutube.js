5a455f2e3666ac658e2f23a7ed541a39fa8f52dc
PygmalionPolymorph <pygmalionpolymorph@gmail.com>
2018-04-12 23:51:43 +0200

    Add lots of basic functionality

---
+++ src/lib/randomYoutube.js
@@ -0,0 +1,9 @@
+import { prop } from 'ramda';
+
+const randomYoutubeAPIURL = `http://randomyoutube.net/api/getvid?api_token=${process.env.RANDOMYOUTUBE_API_KEY}`;
+const corsProxyURL = `http://${process.env.CORS_HOST}:${process.env.CORS_PORT}/`;
+
+export const fetchRandomVideo = () => fetch(`${corsProxyURL}${randomYoutubeAPIURL}`)
+  .then(res => res.json())
+  .then(prop('vid'));
+
---
+++ src/view/verflixxteklixx/player.js
@@ -0,0 +1,14 @@
+import m from 'mithril';
+import { updatePlayerEstimate } from '../../state/verflixxteklixx';
+
+const changeEstimate = player => (e) => {
+  updatePlayerEstimate(player, e.target.value);
+};
+
+export default {
+  view: ({ attrs: { player }}) => m('.player', [
+    m('.player__name', player.name),
+    m('input[type="number"]', { onchange: changeEstimate(player) }),
+    m('.player__score', player.score)
+  ]),
+};
