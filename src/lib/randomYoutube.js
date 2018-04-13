591f6ba8bbc3c06fb049a0cde56593680bddbcfb
PygmalionPolymorph <pygmalionpolymorph@gmail.com>
2018-04-13 18:01:47 +0200

    Use https

--- src/lib/randomYoutube.js
+++ src/lib/randomYoutube.js
@@ -1,7 +1,7 @@
 import { prop } from 'ramda';
 
-const randomYoutubeAPIURL = `http://randomyoutube.net/api/getvid?api_token=${process.env.RANDOMYOUTUBE_API_KEY}`;
-const corsProxyURL = `http://${process.env.CORS_HOST}:${process.env.CORS_PORT}/`;
+const randomYoutubeAPIURL = `https://randomyoutube.net/api/getvid?api_token=${process.env.RANDOMYOUTUBE_API_KEY}`;
+const corsProxyURL = `https://${process.env.CORS_HOST}:${process.env.CORS_PORT}/`;
 
 export const fetchRandomVideo = () => fetch(`${corsProxyURL}${randomYoutubeAPIURL}`)
   .then(res => res.json())
